6c60fc1428a1e303d1770fbdb0e080526134868f
zaki987 <zaki.work1@gmail.com>
2021-04-09 13:49:01 +0000

    Revert "kernel: Use task struct instead of pid to check for zygote"
    This reverts commit 5e7a0239bdddb2fcbe771662f955800dd8b6d4ae.

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -2945,7 +2945,7 @@ static ssize_t __cgroup_procs_write(struct kernfs_open_file *of, char *buf,
 	/* This covers boosting for app launches and app transitions */
 	if (!ret && !threadgroup &&
 	    !strcmp(of->kn->parent->name, "top-app") &&
-	    task_is_zygote(tsk->parent))
+	    is_zygote_pid(tsk->parent->pid))
 		cpu_input_boost_kick_max(1000);
 
 	put_task_struct(tsk);
